532b2126bda2a7efc45897cffe56baf1f2527bf1
maxxitmen <61408791+maxxitmen@users.noreply.github.com>
2024-04-17 19:45:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+name1 = "Это моя строка"
+name2 = "Это новая строка "
+print (name1[0]);
+print (name1[-1]);
+print (name1[2:5]);
+print (name1[::-1]);
+print (len(name1));
+print (name2 + name1);
+
+
